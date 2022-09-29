f26bd1d2316706305630544e293c8bc90cac527d
K3ARN3Y <5862727+K3ARN3Y@users.noreply.github.com>
2022-09-29 15:58:50 -0700

    Add a todo and update numbers

--- recipes/api.js
+++ recipes/api.js
@@ -29,6 +29,7 @@ router.get("/search", async function (req, res) {
   // TODO 2: return recipe_id, title, and the first photo as url
   // for recipes without photos, return url as default.jpg
 
+  // TODO 3: edit the response since we should now have the data to return
   res.status(501).json({ status: "not implemented", rows: [] });
 });
 
@@ -36,23 +37,24 @@ router.get("/get", async (req, res) => {
   const recipeId = req.query.id ? +req.query.id : 1;
   console.log("recipe get", recipeId);
 
-  // TODO 3: 
+  // TODO 4: 
   // return all ingredient rows as ingredients
   //    name the ingredient image `ingredient_image`
   //    name the ingredient type `ingredient_type`
   //    name the ingredient title `ingredient_title`
   //
   //
-  // TODO 4: 
+  // TODO 5: 
   // return all photo rows as photos
   //    return the title, body, and url (named the same)
   //
   //
-  // TODO 5:
+  // TODO 6:
   // return the title as title
   // return the body as body
   // if no row[0] has no photo, return it as default.jpg
 
+  // TODO 7: edit the response since we should now have the data to return
   res.status(501).json({ status: "not implemented" });
 });
 /**
