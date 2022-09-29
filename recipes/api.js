bcb9121fb943e7c70d6b38dc5dfae4e54fb3698f
K3ARN3Y <5862727+K3ARN3Y@users.noreply.github.com>
2022-09-29 12:34:40 -0700

    Add todo comments to recipe/api

--- recipes/api.js
+++ recipes/api.js
@@ -21,13 +21,12 @@ router.get("/detail", (_, res) =>
  * Student code starts here
  */
 
-// connect to postgres
+// TODO 1: connect to postgres
 
 router.get("/search", async function (req, res) {
   console.log("search recipes");
 
-  // return recipe_id, title, and the first photo as url
-  //
+  // TODO 2: return recipe_id, title, and the first photo as url
   // for recipes without photos, return url as default.jpg
 
   res.status(501).json({ status: "not implemented", rows: [] });
@@ -37,16 +36,19 @@ router.get("/get", async (req, res) => {
   const recipeId = req.query.id ? +req.query.id : 1;
   console.log("recipe get", recipeId);
 
+  // TODO 3: 
   // return all ingredient rows as ingredients
   //    name the ingredient image `ingredient_image`
   //    name the ingredient type `ingredient_type`
   //    name the ingredient title `ingredient_title`
   //
   //
+  // TODO 4: 
   // return all photo rows as photos
   //    return the title, body, and url (named the same)
   //
   //
+  // TODO 5:
   // return the title as title
   // return the body as body
   // if no row[0] has no photo, return it as default.jpg
