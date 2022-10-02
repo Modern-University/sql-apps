80d71ebfbfc6612d9ff3885cff237d540b3f887f
K3ARN3Y <5862727+K3ARN3Y@users.noreply.github.com>
2022-10-01 19:19:48 -0700

    Add movies pages for exercises

---
+++ movies/index.sql
@@ -0,0 +1,9 @@
+-- Challenge 1: Which movie made the most money?
+
+-- Challenge 2: How much revenue did the movies Keanu Reeves act in make?
+
+-- Challenge 3: Which 5 people were in the movies that had the most revenue?
+
+-- Challenge 4: Which 10 movies have the most keywords?
+
+-- Challenge 5: Which category is associated with the most movies
